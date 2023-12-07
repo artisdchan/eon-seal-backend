2390feab498f71f3eaeb4f5fab2cd7a530136c9f
artisd.cha <>
2023-12-07 19:22:22 +0700

    test

--- index.js
+++ index.js
@@ -26,18 +26,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const index_1 = __importDefault(require("./src/index"));
-const dotenv = __importStar(require("dotenv"));
+var express_1 = __importDefault(require("express"));
+var index_1 = __importDefault(require("./src/index"));
+var dotenv = __importStar(require("dotenv"));
+require("reflect-metadata");
 dotenv.config();
-const app = (0, express_1.default)();
-const server = new index_1.default(app);
-const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+var app = (0, express_1.default)();
+var server = new index_1.default(app);
+var PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 app
-    .listen(PORT, "localhost", function () {
-    console.log(`Server is running on port ${PORT}.`);
+    .listen(PORT, function () {
+    console.log("Server is running on port ".concat(PORT, "."));
 })
-    .on("error", (err) => {
+    .on("error", function (err) {
     if (err.code === "EADDRINUSE") {
         console.log("Error: address already in use");
     }
