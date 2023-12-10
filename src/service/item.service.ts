4b2d5f5a1db5b404fea5ea80fc7686c0886c8116
artisd.cha <>
2023-12-10 12:43:35 +0700

    edit add item

--- src/service/item.service.ts
+++ src/service/item.service.ts
@@ -11,21 +11,16 @@ export default class ItemService {
             await ItemDataSource.initialize();
         }
         
-        const cashItem = await ItemDataSource.manager.findOneBy(SealItem, { userId: userId, itemId: itemId });
-        if (cashItem == null) {
-            await ItemDataSource.manager.save(SealItem, {
-                itemId: itemId,
-                ItemOp1: itemAmount - 1,
-                ItemOp2: 0,
-                ItemLimit: 0,
-                userId: userId,
-                OwnerDate: new Date,
-                bxaid: 'BUY'
-            });
-        } else {
-            cashItem.ItemOp1 += itemAmount;
-            await ItemDataSource.manager.save(SealItem, cashItem);
-        }
+        // const cashItem = await ItemDataSource.manager.findOneBy(SealItem, { userId: userId, itemId: itemId });
+        await ItemDataSource.manager.save(SealItem, {
+            itemId: itemId,
+            ItemOp1: itemAmount - 1,
+            ItemOp2: 0,
+            ItemLimit: 0,
+            userId: userId,
+            OwnerDate: new Date,
+            bxaid: 'BUY'
+        });
 
 
         return "";
