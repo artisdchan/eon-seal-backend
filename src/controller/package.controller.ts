a596822e8b02bafb0ed04169c82a1efb3d9066f4
artisd.cha <>
2023-12-13 18:27:49 +0700

    fix bug

--- src/controller/package.controller.ts
+++ src/controller/package.controller.ts
@@ -215,7 +215,8 @@ export class PackageController {
 
                 historyEntity.status = PackageHistoryStatus.DONE
                 webUserDetailEntity.topupCredit -= packageEntity.priceTopupCredit
-                await ItemDataSource.manager.save([historyEntity, webUserDetailEntity])
+                await ItemDataSource.manager.save(historyEntity)
+                await SealMemberDataSource.manager.save(webUserDetailEntity)
 
                 return res.status(200).json({ status: 200, data: { puchaseId: historyEntity.purchaseId } })
 
