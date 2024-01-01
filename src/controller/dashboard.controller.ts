b7b5160a5f99ab24d6b3433b92227fbdecdf12cf
artisd.cha <>
2024-01-01 22:18:21 +0700

    test

--- src/controller/dashboard.controller.ts
+++ src/controller/dashboard.controller.ts
@@ -494,7 +494,7 @@ export class DashboardController {
             const storeService = new StoreService();
             const storeEntity = await GDB0101DataSource.manager.find(store);
             for (let each of storeEntity) {
-                const itemPos = storeService.getBugPos(28000, each)
+                const itemPos = storeService.getBugPos(29000, each)
                 for (let eachPos of itemPos) {
                     type ObjectKey = keyof typeof store;
 
